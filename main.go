8e33dc9ece36ae5ac43dde39340d90c519c6a577
Eslam Mostafa <cseslam@gmail.com>
2017-10-06 02:01:01 +0200

    Fix: inject snippets routes to the main router

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/ignoshi/core/bookmarks"
+	"github.com/ignoshi/core/snippets"
 	"github.com/ignoshi/core/tags"
 )
 
@@ -14,6 +15,7 @@ func main() {
 	r := mux.NewRouter()
 	tags.InjectRoutes(r.PathPrefix("/api/tags").Subrouter())
 	bookmarks.InjectRoutes(r.PathPrefix("/api/bookmarks").Subrouter())
+	snippets.InjectRoutes(r.PathPrefix("/api/snippets").Subrouter())
 
 	srv := &http.Server{
 		Handler:      r,
